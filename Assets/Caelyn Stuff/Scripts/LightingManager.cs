51d713906831b62f1c11afea9e5b33ed0821458c
kei-exe <icrystalxd@gmail.com>
2025-02-21 09:38:22 +0800

    added daynight cycle and made weapon dmg serializable field

---
+++ Assets/Caelyn Stuff/Scripts/LightingManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class LightingManager : MonoBehaviour
+{
+    // References
+    [SerializeField] private Light directionalLight;
+    [SerializeField] private LightingPreset preset;
+    // Variables
+    [SerializeField, Range(0, 24)] private float time;
+
+    private void Update()
+    {
+        if (preset == null)
+            return;
+
+        if (Application.isPlaying)
+        {
+            time += Time.deltaTime;
+            time %= 24; // Clamp between 0-24
+            UpdateLighting(time / 24f);
+        }
+        else
+        {
+            UpdateLighting(time / 24f);
+        }
+    }
+
+    private void UpdateLighting(float timePercent)
+    {
+        RenderSettings.ambientLight = preset.ambientColor.Evaluate(timePercent);
+        RenderSettings.fogColor = preset.fogColor.Evaluate(timePercent);
+
+        if (directionalLight != null)
+        {
+            directionalLight.color = preset.directionalColor.Evaluate(timePercent);
+            directionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f) - 90f, 170f, 0));
+        }
+    }
+
+    private void OnValidate()
+    {
+        if (directionalLight != null)
+            return;
+
+        if (RenderSettings.sun != null)
+            directionalLight = RenderSettings.sun;
+        else
+        { 
+            Light[] lights = GameObject.FindObjectsOfType<Light>();
+            foreach(Light light in lights)
+            {
+                if (light.type == LightType.Directional)
+                {
+                    directionalLight = light;
+                    return;
+                }
+            }
+        }
+
+    }
+}
