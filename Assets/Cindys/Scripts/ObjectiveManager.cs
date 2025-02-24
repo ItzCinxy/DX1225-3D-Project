2e72e0ed6ed562b63fc76f6d08bd384a290627f4
kei-exe <icrystalxd@gmail.com>
2025-02-24 16:50:12 +0800

    quest ui done

--- Assets/Cindys/Scripts/ObjectiveManager.cs
+++ Assets/Cindys/Scripts/ObjectiveManager.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.ProBuilder.Shapes;
 
@@ -5,6 +6,8 @@ public class ObjectiveManager : MonoBehaviour
 {
     public static ObjectiveManager Instance;
     public GameObject mainDoor; // Assign the door in the Inspector
+    [SerializeField] private TMP_Text questKeyDisplay;
+    [SerializeField] private TMP_Text questZombieDisplay;
 
     private bool hasKey = false;
     private int zombiesKilled = 0;
@@ -17,17 +20,22 @@ public class ObjectiveManager : MonoBehaviour
             Instance = this;
         else
             Destroy(gameObject);
+
+        if (questKeyDisplay != null) questKeyDisplay.text = $"Key collected: {hasKey}";
+        if (questZombieDisplay != null) questZombieDisplay.text = $"Zombies killed: {zombiesKilled} / {zombiesToKill}";
     }
 
     public void PickUpKey()
     {
         hasKey = true;
+        questKeyDisplay.text = $"Key collected: {hasKey}";
         CheckObjectives();
     }
 
     public void ZombieKilled()
     {
         zombiesKilled++;
+        questZombieDisplay.text = $"Zombies killed: {zombiesKilled} / {zombiesToKill}";
         CheckObjectives();
     }
 
@@ -36,6 +44,8 @@ public class ObjectiveManager : MonoBehaviour
         if (hasKey && zombiesKilled >= zombiesToKill && !objectivesCompleted)
         {
             objectivesCompleted = true;
+            questKeyDisplay.text = "Objectives completed!\n Door is opened.";
+            questZombieDisplay.text = "";
             OpenDoor();
         }
     }
