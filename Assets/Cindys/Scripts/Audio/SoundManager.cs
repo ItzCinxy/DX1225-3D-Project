fb8db4bf20f5a5ece80b6360f018b1c95ac72660
ItzCinxy <itzcinx2005@gmail.com>
2025-02-27 18:53:55 +0800

    yes

--- Assets/Cindys/Scripts/Audio/SoundManager.cs
+++ Assets/Cindys/Scripts/Audio/SoundManager.cs
@@ -26,8 +26,9 @@ public class SoundManager : MonoBehaviour
     public AudioClip ZombieAttack;
     public AudioClip ZombieHurt;
     public AudioClip ZombieDie;
+    public AudioClip ZombieExplodeDie;
 
-    public AudioClip EXplodeZombieDie;
+    public AudioClip AfterExplodeZombieDie;
 
     private bool isPaused = false;
 
